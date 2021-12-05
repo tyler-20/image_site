a4383c542fa089a15aeafcf42e08d36406b03280
Tyler <tyler.mansell20@gmail.com>
2021-12-04 17:17:59 -0700

    updated port

--- back-end/server.js
+++ back-end/server.js
@@ -178,4 +178,4 @@ app.get("/api/likedimages/:user", async (req, res) => {
 });
 
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3001, () => console.log('Server listening on port 3001!'));
